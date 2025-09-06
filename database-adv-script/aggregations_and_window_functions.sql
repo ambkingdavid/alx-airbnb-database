beef13701ffe353358e46f379f9c1717f7523fee
ambkingdavid <kelvinwooldbridge@gmail.com>
2025-09-06 12:39:58 +0100

    window function update

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,6 +11,7 @@ ORDER BY total_bookings DESC;
 SELECT 
     property_id,
     booking_count,
+    ROW_NUMBER() OVER (ORDER BY booking_count DESC) AS row_number,
     RANK() OVER (ORDER BY booking_count DESC) AS booking_rank
 FROM (
     SELECT 
@@ -18,4 +19,4 @@ FROM (
         COUNT(*) AS booking_count
     FROM bookings
     GROUP BY property_id
-) AS property_bookings;
+) AS property_stats;
