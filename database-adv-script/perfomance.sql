535ccf627f914e908045e80d26b2f57c821c6e43
ambkingdavid <kelvinwooldbridge@gmail.com>
2025-09-06 13:22:07 +0100

    update optimisation

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,25 +1,46 @@
 -- Initial Query: Retrieve bookings with user, property, and payment details
+-- Initial query with WHERE, AND, and EXPLAIN ANALYZE
+-- Optimized query with WHERE and AND clauses
 SELECT 
     b.booking_id,
     b.start_date,
     b.end_date,
     b.total_price,
     b.status,
-    u.user_id,
     u.first_name,
     u.last_name,
-    u.email,
-    p.property_id,
     p.name AS property_name,
-    p.location,
-    pay.payment_id,
     pay.amount,
     pay.payment_method
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
 LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND b.start_date >= CURRENT_DATE - INTERVAL '30 days'
 ORDER BY b.start_date DESC;
+
+-- Analyze performance of a complex query using EXPLAIN
+EXPLAIN ANALYZE
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.first_name,
+    u.last_name,
+    p.name AS property_name,
+    pay.amount,
+    pay.payment_method
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+  AND b.start_date >= CURRENT_DATE - INTERVAL '30 days'
+ORDER BY b.start_date DESC;
+
 
 
 -- Refactored Query
